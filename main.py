e419bc34e2ec88eb62845d74da3fcb4c83f6cc72
ericaf7923 <72775447+ericaf7923@users.noreply.github.com>
2021-01-21 20:08:53 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+#while True
+    #print("sound level :" +input.sound_level())
+    #if input.sound_level()>5
+    #lights.set_all(lights.rgb(255, 0, 255))
+    #else: 
+        #light.set_all(light.rgb(255, 0, 255))
 while True: 
-
-    if input.light_level() >5:
-        music.ba_ding.play_until_done()
-
-    if input.sound_level() > 540:
-        #light.set_all(light.rgb(1, 1, 1))
+    print("light level: " + str(input.light_level()))
+    if input.light_level() >11: 
+        music.play_melody("G E G GE G GE AGG E G E G GE G GE AGG E", 60)
+        music.set_volume(10)
+    else : 
+        music.stop_all_sounds()
+        
--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
+// while True
+// print("sound level :" +input.sound_level())
+// if input.sound_level()>5
+// lights.set_all(lights.rgb(255, 0, 255))
+// else: 
+// light.set_all(light.rgb(255, 0, 255))
 while (true) {
-    if (input.lightLevel() > 5) {
-        music.baDing.playUntilDone()
-    }
-    
-    if (input.soundLevel() > 540) {
-        light.setAll(1)
+    console.log("light level: " + ("" + input.lightLevel()))
+    if (input.lightLevel() > 11) {
+        music.playMelody("G E G GE G GE AGG E G E G GE G GE AGG E", 60)
+        music.setVolume(10)
+    } else {
+        music.stopAllSounds()
     }
     
 }
